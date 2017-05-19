fc04f55195a63a19d2af09678967ccfc5f72f33f
Preslav Anev <preslav.anev@sirma.bg>
2017-05-19 14:17:56 +0300

    Update gitTag

--- gitTag.sh
+++ gitTag.sh
@@ -6,10 +6,11 @@ setup_git() {
 }
 
 tag_master() {
+    - if [ -n "$GH_TOKEN" ] then $GH_TOKEN fi
     - export GIT_TAG=0.1.$TRAVIS_BUILD_NUMBER
     - echo $GIT_TAG
-    - git tag -a $GIT_TAG -m 'Version $VERS'
-    - git push --tags
+    - git tag -a $GIT_TAG -m 'Version $GIT_TAG'
+    - git push --quiet https://$GH_TOKEN@github.com/preslav-anev/test-ris-php-sdk.git --tags
 }
 
 setup_git
