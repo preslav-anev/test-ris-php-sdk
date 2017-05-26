b2431064dc96d952359a243620a2660ee3589179
Preslav Anev <preslav.anev@sirma.bg>
2017-05-26 16:12:45 +0300

    6.7.3 git_tag

--- release.sh
+++ release.sh
@@ -22,10 +22,12 @@ timestamp() {
 # Timestamp variable to receive result from timestamp().
 TIMESTAMP=$(timestamp)
 
+echo $TIMESTAMP
+
 # Generating the .zip archive deliverable with a timestamp and a version number in the root directory of the project.
 # Note : If you are running a Windows operating system you have to install .zip package to git bash(assuming that is what you are using for running this script)
 # You can do that via GoW : https://github.com/bmatzelle/gow/wiki . Here you can find a description of GoW and detailed guide on how to add packages to your git bash console.
-zip -r "Sdk-Ris-Php.zip" . -x vendor/\* -x build/\* -x phpdocs/\* -x .git/\* $FOLDER
+zip -r "Sdk-Ris-Php-$VERS.zip" . -x vendor/\* -x build/\* -x .git/\* $FOLDER
 
 if [ $? -ne 0 ]
 then
