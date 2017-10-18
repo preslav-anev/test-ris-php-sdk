9bc97924693708a688daa1487d5023669ea35a0a
preslav-anev <preslav.anev@sirma.bg>
2017-10-18 17:06:54 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
 
-alert("hello there");
 console.log("hello there");
+
+var url = "https://sandbox01.kaxsdc.com/collect/sdk?m=999666&s=hq35565mtf95bfoecl0s67ek154o8q2d";
+var script = document.createElement('script');
+
+script.src =  url;
+script.type = "text/javascript";
+
+document.head.appendChild(script);
 
