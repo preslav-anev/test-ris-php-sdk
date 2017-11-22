b60b8b323c192b2719a7b1dc25c8e1c2ca4fbe99
preslav-anev <preslav.anev@sirma.bg>
2017-11-22 10:55:31 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,24 @@ var cookies = document.cookie;
 var session = cookies.split(';')[0].split("-").join("").split("=");
 var sessionId = session[1];
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+getCookie("cart");
+
 console.log('cookies: ', JSON.parse(cookies));
 
 var url = "https://sandbox01.kaxsdc.com/collect/sdk?m=999666&s=" + sessionId;
