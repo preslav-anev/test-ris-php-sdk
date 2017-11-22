5246bd6579727b09c0125d0c4b4b1e61ff515033
preslav-anev <preslav.anev@sirma.bg>
2017-11-22 10:21:01 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,14 @@
 
-console.log("hello there");
+var cookies = document.cookie;
+var session = cookies.split(';')[0].split("-").join("").split("=");
+var sessionId = session[1];
 
-var url = "https://sandbox01.kaxsdc.com/collect/sdk?m=999666&s=hq35565mtf95bfoecl0s67ek154o8q2d";
+console.log('session: ', sessionId);
+
+var url = "%s?m=%s&s=" + sessionId;
+
 var script = document.createElement('script');
-
 script.src =  url;
 script.type = "text/javascript";
 
-var shop = Shopify;
-
-console.log('shop: ', shop);
-
 document.head.appendChild(script);
-
-var cookies = document.cookie;
-
-console.log('cookies: ' + cookies);
-
-var session = cookies.split(';')[0].split("-").join("").split("=");
-
-var sessionId = session[1];
-console.log('session: ', sessionId);
